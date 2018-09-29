5a21fd2bb2444fa9f89ad58f073f5044d91217ae
dididi <1031700114@qq.com>
2018-09-29 17:28:25 +0800

    可以实现分组 细节还需考虑一下

--- pocket-ec/src/main/java/com/dididi/pocket/ec/test/adapter/ShopCartAdapter.java
+++ pocket-ec/src/main/java/com/dididi/pocket/ec/test/adapter/ShopCartAdapter.java
@@ -25,7 +25,7 @@ public class ShopCartAdapter extends BaseSectionQuickAdapter<Shop,BaseViewHolder
 
     @Override
     protected void convertHead(BaseViewHolder helper, Shop item) {
-        helper.setText(R.id.test_shop_card_shop_name,item.getShopName());
+        helper.setText(R.id.test_shop_card_shop_name,item.header);
     }
 
     @Override
@@ -33,7 +33,7 @@ public class ShopCartAdapter extends BaseSectionQuickAdapter<Shop,BaseViewHolder
         helper.setText(R.id.test_goods_card_goods_name,item.t.getGoodsName())
                 .setText(R.id.test_goods_card_goods_price,String.valueOf(item.t.getGoodsPrice()))
                 .setText(R.id.test_goods_card_goods_style,item.t.getGoodsStyle())
-                .setText(R.id.test_goods_card_goods_number,item.t.getGoodsCount());
+                .setText(R.id.test_goods_card_goods_number,String.valueOf(item.t.getGoodsCount()));
         GlideApp.with(mContext)
                 .load(Integer.parseInt(item.t.getGoodsImg()))
                 .into((ImageView) helper.getView(R.id.test_goods_card_goods_img));
